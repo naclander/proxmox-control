90ad548d10961f43efaa17a217d8e2ec61eb9748
Nathan Aclander <nathan.aclander@gmail.com>
2023-03-04 11:53:51 -0800

    first commit

---
+++ container.py
@@ -0,0 +1,16 @@
+import click
+import subprocess
+
+import proxmox
+
+
+def run_command(container, command):
+    if isdigit(container):
+        container_id = container
+    else:
+        containers = proxmox.list_proxmox_containers()
+        container_id = containers[container]
+
+    click.echo("Running command")
+    cmd_str = f"pct exec {container_id} -- {command}"
+    proxmox.run_command_interactive(cmd_str)
